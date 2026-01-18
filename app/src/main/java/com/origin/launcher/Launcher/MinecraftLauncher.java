7be05973a5d8f67f1c901b6f65e684f851417e1b
vcx <ryansk0987@gmail.com>
2026-01-18 17:04:07 +0530

    update MinecraftLauncher.java

--- app/src/main/java/com/origin/launcher/Launcher/MinecraftLauncher.java
+++ app/src/main/java/com/origin/launcher/Launcher/MinecraftLauncher.java
@@ -147,7 +147,7 @@ if (!shouldLoadPlayFab(version)) {
                     gameManager.loadLibrary("MediaDecoders_Android");
                     gameManager.loadLibrary("minecraftpe");
                     gameManager.loadLibrary("xelo");
-                    gameManager.loadLibrary("mtbinloader2");
+                    gameManager.loadLibrary("libmtbinloader2");
                 }
                 ModNativeLoader.loadEnabledSoMods(ModManager.getInstance(), context.getCacheDir());
 
